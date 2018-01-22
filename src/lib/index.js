899491609f03dfa953b0277779fb866a0422fdb8
Gabriel C <gabriel.cioropina@altex.ro>
2018-01-22 11:07:20 +0200

    hapi17 plugin

---
+++ src/lib/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const Joi = require('joi');
+const PandaEs = require('panda-es-orm');
+
+// define a log tag
+const LogTag = 'PandaEsORM';
+
+// define validation schemas for loggers
+const esConnectionSchema = Joi.object({
+    host: Joi.any().default('http://localhost:9200/').required()
+}).unknown();
+const loggerSchema = Joi.object({
+    level: Joi.number().default(10).required()
+}).strict();
+
+module.exports = {
+
+    /**
+     * Register method.
+     * @param server
+     * @param options
+     * @returns {Promise.<void>}
+     */
+    async register (server, options) {
+
+        // Reference: https://github.com/replsv/panda-es-orm#configuration
+        options.elasticsearch = await esConnectionSchema.validate(options.elasticsearch);
+        options.logger = await loggerSchema.validate(options.logger);
+
+        const connect = async (params) => {
+
+            const exposed = new PandaEs.orm(params);
+
+            server.decorate('server', 'pandaEs', PandaEs); // expose the entire plugin
+            server.decorate('server', 'pandaEsOrm', exposed); // expose only the ORM instance
+
+            server.log([LogTag, 'info'], 'ES connection created | ' + JSON.stringify(exposed.options));
+        };
+
+        server.events.on('stop', () => {
+
+            server.log([LogTag, 'info'], 'Server stopped');
+        });
+
+        connect(options);
+    },
+
+    /**
+     * Export package.
+     */
+    pkg: require('../../package.json')
+};
